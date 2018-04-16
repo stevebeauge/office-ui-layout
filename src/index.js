7670cbcb1a237a036ed209cb896cce2279944100
blrchen <blrchen@hotmail.com>
2018-04-16 23:48:15 +0800

    Fix icon not shown

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { initializeIcons } from '@uifabric/icons';
 
 import './_styles/index.css';
 
+// Register icons and pull the fonts from the default SharePoint cdn
+initializeIcons();
 ReactDOM.render(
   <App />,
   document.getElementById('root')
